65bfc27958c82866f1c154b39c6efae96a6e1c68
Alex Davies <alex@codecutout.com>
2017-11-12 19:32:09 +0000

    Add build server configuration.

--- build/package.bat
+++
@@ -1,2 +0,0 @@
-dotnet build ..\src\JsonApiSerializer\JsonApiSerializer.csproj --configuration=Release
-dotnet pack ..\src\JsonApiSerializer\JsonApiSerializer.csproj --configuration=Release --output ../../artifacts
