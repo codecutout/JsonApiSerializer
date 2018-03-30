51bbf879cc504d1a6aa07cc49cad1e608a227cf7
jonnii <me@jonnii.com>
2018-03-29 20:07:39 -0400

    [Feature] Ability to build anonymous relationships
    Adds a static `Relationship` class to you can create relationships from
    anonymous types:
    
    ```
    var relationship = Relationship.Create(new
      {
         id = "bobby",
         type = "bandit"
      }
    );
    ```

--- src/JsonApiSerializer/JsonApi/Relationship.cs
+++ src/JsonApiSerializer/JsonApi/Relationship.cs
@@ -1,12 +1,23 @@
 ﻿namespace JsonApiSerializer.JsonApi
 {
+    public static class Relationship
+    {
+        public static Relationship<TData> Create<TData>(TData data)
+        {
+            return new Relationship<TData>
+            {
+                Data = data
+            };
+        }
+    }
+
     /// <summary>
     /// Represents a Relationship.
     /// </summary>
     /// <typeparam name="TData">The type of the data.</typeparam>
     public class Relationship<TData>
     {
-        public TData Data {get; set;}
+        public TData Data { get; set; }
 
         public Links Links { get; set; }
 
