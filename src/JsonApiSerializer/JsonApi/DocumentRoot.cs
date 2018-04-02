73943cac775b977d9472735ca4925bfc41e00897
Jonathan Goldman <me@jonnii.com>
2018-04-02 15:59:05 -0400

    [Feature] Ability to build anonymous relationships (#46)
    Ability to build anonymous `DocumentRoot` and `Relationship`
    
    Adds a static `DocumentRoot` and `Relationship` class to you can create relationships from
    anonymous types:
    
    ```
    var relationship = DocumentRoot.Create(new
      {
         id = "bobby",
         type = "bandit"
      }
    );
    var relationship = Relationship.Create(new
      {
         id = "bobby",
         type = "bandit"
      }
    );
    ```

--- src/JsonApiSerializer/JsonApi/DocumentRoot.cs
+++ src/JsonApiSerializer/JsonApi/DocumentRoot.cs
@@ -5,14 +5,29 @@ using Newtonsoft.Json;
 
 namespace JsonApiSerializer.JsonApi
 {
+    public static class DocumentRoot
+    {
+        /// <summary>
+        /// Creates a new DocumentRoot for a given type.
+        /// </summary>
+        /// <typeparam name="TData">The type of the data.</typeparam>
+        public static DocumentRoot<TData> Create<TData>(TData data)
+        {
+            return new DocumentRoot<TData>
+            {
+                Data = data
+            };
+        }
+    }
+
     /// <summary>
     /// Represents the root of a JsonApi document
     /// </summary>
     /// <typeparam name="TData">The type of the data.</typeparam>
-    /// <seealso cref="JsonApiSerializer.JsonApi.WellKnown.IDocumentRoot{TData}" />
+    /// <seealso cref="WellKnown.IDocumentRoot{TData}" />
     public class DocumentRoot<TData> : IDocumentRoot<TData>
     {
-        [JsonProperty(propertyName: "jsonapi", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonProperty("jsonapi", NullValueHandling = NullValueHandling.Ignore)]
         public VersionInfo JsonApi { get; set; }
 
         public TData Data { get; set; }
--- src/JsonApiSerializer/JsonApi/Relationship.cs
+++ src/JsonApiSerializer/JsonApi/Relationship.cs
@@ -1,12 +1,27 @@
 ﻿namespace JsonApiSerializer.JsonApi
 {
+    public static class Relationship
+    {
+        /// <summary>
+        /// Creates a Relationship for a given type.
+        /// </summary>
+        /// <typeparam name="TData">The type of the data.</typeparam>
+        public static Relationship<TData> Create<TData>(TData data)
+        {
+            return new Relationship<TData>
+            {
+                Data = data
+            };
+        }
+    }
+
     /// <summary>
     /// Represents a Relationship.
     /// </summary>
     /// <typeparam name="TData">The type of the data.</typeparam>
     public class Relationship<TData>
     {
-        public TData Data {get; set;}
+        public TData Data { get; set; }
 
         public Links Links { get; set; }
 
