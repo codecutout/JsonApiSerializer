62ec34d9faa0af9a5678cdbb398e29c2b3a0f657
jonnii <me@jonnii.com>
2018-04-02 13:49:49 -0400

    comments and document root

--- src/JsonApiSerializer/JsonApi/Relationship.cs
+++ src/JsonApiSerializer/JsonApi/Relationship.cs
@@ -2,6 +2,10 @@
 {
     public static class Relationship
     {
+        /// <summary>
+        /// Creates a Relationship for a given type.
+        /// </summary>
+        /// <typeparam name="TData">The type of the data.</typeparam>
         public static Relationship<TData> Create<TData>(TData data)
         {
             return new Relationship<TData>
