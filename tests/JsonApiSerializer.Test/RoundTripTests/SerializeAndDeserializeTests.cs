06519585aff4812350d0f4158acf23baffdf1e2f
jonnii <me@jonnii.com>
2018-04-10 22:46:01 -0400

    More crossplatform support
     * Uses json comparer in attribute tests
     * Updates AutoFixture
     * Targets netcoreapp2.0 for tests

--- tests/JsonApiSerializer.Test/RoundTripTests/SerializeAndDeserializeTests.cs
+++ tests/JsonApiSerializer.Test/RoundTripTests/SerializeAndDeserializeTests.cs
@@ -1,13 +1,13 @@
 ﻿using JsonApiSerializer.Test.Models.Articles;
 using Newtonsoft.Json;
-using Ploeh.AutoFixture;
+using AutoFixture;
 using Xunit;
 
 namespace JsonApiSerializer.Test.RoundTripTests
 {
     public class SerializeAndDeserializeTests
     {
-        public JsonApiSerializerSettings settings = new JsonApiSerializerSettings()
+        public JsonApiSerializerSettings settings = new JsonApiSerializerSettings
         {
             Formatting = Formatting.Indented //pretty print makes it easier to debug
         };
@@ -15,9 +15,8 @@ namespace JsonApiSerializer.Test.RoundTripTests
         [Fact]
         public void When_serialize_should_deserialize_to_equal_objects()
         {
-            var fixture = new Ploeh.AutoFixture.Fixture();
+            var fixture = new Fixture();
             var product1 = fixture.Create<Article[]>();
-
 
             var json = JsonConvert.SerializeObject(product1, settings);
             var product2 = JsonConvert.DeserializeObject<Article[]>(json, settings);
@@ -25,7 +24,6 @@ namespace JsonApiSerializer.Test.RoundTripTests
             var equal = TestUtils.JsonObjectEqualityComparer<Article[]>.Instance.Equals(product1, product2);
 
             Assert.True(equal);
-
         }
     }
 }
