c598fc40d73642aee47e12f3546f40e594710ccf
Alex Davies <alex@codecutout.com>
2019-01-09 21:25:32 +0000

    Added support for json.net conditional serialization

---
+++ tests/JsonApiSerializer.Test/Models/Articles/ArticleWithSerializationConditions.cs
@@ -0,0 +1,54 @@
+﻿using JsonApiSerializer.JsonApi;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace JsonApiSerializer.Test.Models.Articles
+{
+    public class ArticleWithSerializationConditions
+    {
+        public string Type { get; set; } = "articles";
+
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public PersonWithSerializationConditions Author { get; set; }
+
+        public List<Comment> Comments { get; set; }
+
+        public Links Links { get; set; }
+
+        [JsonIgnore]
+        public List<string> SerializeProperties { get; set; }
+
+        public bool ShouldSerializeType()
+        {
+            return SerializeProperties == null || SerializeProperties.Contains(nameof(Type));
+        }
+
+        public bool ShouldSerializeId()
+        {
+            return SerializeProperties == null || SerializeProperties.Contains(nameof(Id));
+        }
+
+        public bool ShouldSerializeTitle()
+        {
+            return SerializeProperties == null || SerializeProperties.Contains(nameof(Title));
+        }
+
+        public bool ShouldSerializeAuthor()
+        {
+            return SerializeProperties == null || SerializeProperties.Contains(nameof(Author));
+        }
+
+        public bool ShouldSerializeComments()
+        {
+            return SerializeProperties == null || SerializeProperties.Contains(nameof(Comments));
+        }
+
+        public bool ShouldSerializeLinks()
+        {
+            return SerializeProperties == null || SerializeProperties.Contains(nameof(Links));
+        }
+    }
+}
---
+++ tests/JsonApiSerializer.Test/Models/Articles/PersonWithSerializationConditions.cs
@@ -0,0 +1,59 @@
+﻿using JsonApiSerializer.JsonApi;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace JsonApiSerializer.Test.Models.Articles
+{
+    public class PersonWithSerializationConditions
+    {
+        public string Type { get; set; } = "people";
+
+        public string Id { get; set; }
+
+        [JsonProperty(propertyName: "first-name")]
+        public string FirstName { get; set; }
+
+        [JsonProperty(propertyName: "last-name")]
+        public string LastName { get; set; }
+
+        public string Twitter { get; set; }
+
+        public Links Links { get; set; }
+
+        public Meta Meta { get; set; }
+
+        [JsonIgnore]
+        public List<string> SerializeProperties { get; set; }
+
+        public bool ShouldSerializeType()
+        {
+            return SerializeProperties == null || SerializeProperties.Contains(nameof(Type));
+        }
+
+        public bool ShouldSerializeId()
+        {
+            return SerializeProperties == null || SerializeProperties.Contains(nameof(Id));
+        }
+
+        public bool ShouldSerializeFirstName()
+        {
+            return SerializeProperties == null || SerializeProperties.Contains(nameof(FirstName));
+        }
+
+        public bool ShouldSerializeLastName()
+        {
+            return SerializeProperties == null || SerializeProperties.Contains(nameof(LastName));
+        }
+
+        public bool ShouldSerializeTwitter()
+        {
+            return SerializeProperties == null || SerializeProperties.Contains(nameof(Twitter));
+        }
+
+        public bool ShouldSerializeLinks()
+        {
+            return SerializeProperties == null || SerializeProperties.Contains(nameof(Links));
+        }
+
+    }
+}
