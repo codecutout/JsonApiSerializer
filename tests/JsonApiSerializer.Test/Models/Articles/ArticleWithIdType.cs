cb6b17f26373f0038380f650e7af9b1bb7e83394
Alex Davies <alex@codecutout.com>
2019-03-23 23:43:17 +0000

    Added support for integer ids

---
+++ tests/JsonApiSerializer.Test/Models/Articles/ArticleWithIdType.cs
@@ -0,0 +1,20 @@
+﻿using JsonApiSerializer.JsonApi;
+using System.Collections.Generic;
+
+namespace JsonApiSerializer.Test.Models.Articles
+{
+    public class ArticleWithIdType<T>
+    {
+        public string Type { get; set; } = "articles";
+
+        public T Id { get; set; }
+
+        public string Title { get; set; }
+
+        public Person Author { get; set; }
+
+        public List<Comment> Comments { get; set; }
+
+        public Links Links { get; set; }
+    }
+}
--- tests/JsonApiSerializer.Test/RoundTripTests/SerializeAndDeserializeTests.cs
+++ tests/JsonApiSerializer.Test/RoundTripTests/SerializeAndDeserializeTests.cs
@@ -2,6 +2,11 @@
 using Newtonsoft.Json;
 using AutoFixture;
 using Xunit;
+using System;
+using AutoFixture.Kernel;
+using JsonApiSerializer.Test.Models.Locations;
+using JsonApiSerializer.Test.Models.Timer;
+using System.Linq;
 
 namespace JsonApiSerializer.Test.RoundTripTests
 {
@@ -12,16 +17,42 @@ namespace JsonApiSerializer.Test.RoundTripTests
             Formatting = Formatting.Indented //pretty print makes it easier to debug
         };
 
-        [Fact]
-        public void When_serialize_should_deserialize_to_equal_objects()
+        
+        [Theory]
+        [InlineData(typeof(Article[]))]
+        [InlineData(typeof(Article))]
+        [InlineData(typeof(ArticleWithRelationship))]
+        [InlineData(typeof(ArticleWithDatalessRelationship))]
+        [InlineData(typeof(ArticleWithNoType))]
+        [InlineData(typeof(ArticleWithResourceIdentifier))]
+        [InlineData(typeof(ArticleWithReadonlyType))]
+        [InlineData(typeof(ArticleWithIdType<string>))]
+        [InlineData(typeof(ArticleWithIdType<int>))]
+        [InlineData(typeof(ArticleWithIdType<long>))]
+        [InlineData(typeof(ArticleWithIdType<Guid>))]
+        [InlineData(typeof(ArticleWithIdType<uint>))]
+        [InlineData(typeof(ArticleWithIdType<ulong>))]
+        [InlineData(typeof(ArticleWithIdType<int?>))]
+        [InlineData(typeof(ArticleWithIdType<long?>))]
+        [InlineData(typeof(ArticleWithIdType<Guid?>))]
+        [InlineData(typeof(ArticleWithIdType<uint?>))]
+        [InlineData(typeof(ArticleWithIdType<ulong?>))]
+        [InlineData(typeof(Timer))]
+        public void When_serialize_should_deserialize_to_equal_objects(Type type)
         {
             var fixture = new Fixture();
-            var product1 = fixture.Create<Article[]>();
 
-            var json = JsonConvert.SerializeObject(product1, settings);
-            var product2 = JsonConvert.DeserializeObject<Article[]>(json, settings);
+            //handle recursion
+            fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+                .ForEach(b => fixture.Behaviors.Remove(b));
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior(3));
 
-            var equal = TestUtils.JsonObjectEqualityComparer<Article[]>.Instance.Equals(product1, product2);
+            var item1 = new SpecimenContext(fixture).Resolve(type);
+
+            var json = JsonConvert.SerializeObject(item1, settings);
+            var item2 = JsonConvert.DeserializeObject(json, type, settings);
+
+            var equal = TestUtils.JsonObjectEqualityComparer<object>.Instance.Equals(item1, item2);
 
             Assert.True(equal);
         }
