43e1964624d1d7b3e6735d6eb15b127b495ac556
Alex Davies <greenhippo86@gmail.com>
2019-12-16 17:29:48 +0000

    Prevent stack overflow exceptions from occurring with circular references (#114)
    Objects were only being added to the `included` dictionary after completing deserialization. This meant that if relationships were referencing back to the object then they would create new objects.
    
    When we know the contents of the new objects (i.e. `Included` was processed first) this can cause stackoverflow errors. When two objects reference each other, new objects are continuously created as it resolves each relationship
    
    Fix is to add objects to the included list as soon as they are created, before they process any relationships

---
+++ tests/JsonApiSerializer.Test/Models/Products/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonApiSerializer.Test.Models.Products
+{
+    public class Product
+    {
+        public string Type { get; set; } = "product";
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Category Category { get; set; }
+        public Seller[] Sellers { get; set; }
+    }
+}
